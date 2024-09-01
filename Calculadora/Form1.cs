de371e235d6d9c493399fca3ffbea3d95e254555
rovlas4 <44chava44@gmail.com>
2024-09-01 17:21:30 -0600

    Se escribe nuevo número después de presionar signo.

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -12,25 +12,30 @@ namespace Calculadora
 {
     public partial class Form1 : Form
     {
+        List<string> symbolList = new List<string>();
+        int num1 = 0;
+        bool signPressed = false;
+        bool numberPressed = false;
+
         public Form1()
         {
             InitializeComponent();
+            symbolList.Add("+");
+            symbolList.Add("-");
+            symbolList.Add("×");
+            symbolList.Add("÷");
         }
 
         private void zero_Click(object sender, EventArgs e)
         {
+            numberPressed = true;
+
             if (mainDisplay.Text != "0")
                 mainDisplay.Text += "0";
         }
 
         private void mainDisplay_TextChanged(object sender, EventArgs e)
         {
-            List<string> symbolList = new List<string>();
-            symbolList.Add("+");
-            symbolList.Add("-");
-            symbolList.Add("×");
-            symbolList.Add("÷");
-
             if (mainDisplay.Text.StartsWith("0") && mainDisplay.Text.Length > 1)
             {
                 mainDisplay.Text = mainDisplay.Text.Remove(0, 1);
@@ -42,81 +47,100 @@ namespace Calculadora
                 {
                     secondDisplay.Text = mainDisplay.Text;
                     mainDisplay.Text = mainDisplay.Text.Substring(0, mainDisplay.Text.Length-1);
-                }
-                if (secondDisplay.Text.EndsWith(var))
-                {
-
+                    num1 = Int32.Parse(mainDisplay.Text);
                 }
             }
+
+            if (signPressed && numberPressed)
+            {
+                mainDisplay.Text = mainDisplay.Text.Substring(mainDisplay.Text.Length-1, 1);
+                signPressed = false;
+            }
+
+            numberPressed = false;
         }
 
         private void one_Click(object sender, EventArgs e)
         {
+            numberPressed = true;
             mainDisplay.Text += "1";
         }
 
         private void two_Click(object sender, EventArgs e)
         {
+            numberPressed = true;
             mainDisplay.Text += "2";
         }
 
         private void three_Click(object sender, EventArgs e)
         {
+            numberPressed = true;
             mainDisplay.Text += "3";
         }
 
         private void four_Click(object sender, EventArgs e)
         {
+            numberPressed = true;
             mainDisplay.Text += "4";
         }
 
         private void five_Click(object sender, EventArgs e)
         {
+            numberPressed = true;
             mainDisplay.Text += "5";
         }
 
         private void six_Click(object sender, EventArgs e)
         {
+            numberPressed = true;
             mainDisplay.Text += "6";
         }
 
         private void seven_Click(object sender, EventArgs e)
         {
+            numberPressed = true;
             mainDisplay.Text += "7";
         }
 
         private void eight_Click(object sender, EventArgs e)
         {
+            numberPressed = true;
             mainDisplay.Text += "8";
         }
 
         private void nine_Click(object sender, EventArgs e)
         {
+            numberPressed = true;
             mainDisplay.Text += "9";
         }
 
         private void allClear_Click(object sender, EventArgs e)
         {
             mainDisplay.Text = "0";
+            secondDisplay.Text = "";
         }
 
         private void plus_Click(object sender, EventArgs e)
         {
+            signPressed = true;
             mainDisplay.Text += "+";
         }
 
         private void minus_Click(object sender, EventArgs e)
         {
+            signPressed = true;
             mainDisplay.Text += "-";
         }
 
         private void times_Click(object sender, EventArgs e)
         {
+            signPressed = true;
             mainDisplay.Text += "×";
         }
 
         private void divide_Click(object sender, EventArgs e)
         {
+            signPressed = true;
             mainDisplay.Text += "÷";
         }
     }
